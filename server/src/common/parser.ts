c3fbf885cf02322937120670cb415cf7808cb34a
Carey Williams <CareyJWilliams@users.noreply.github.com>
2023-11-12 19:53:11 +0000

    Update server/src/common/parser.ts
    Co-authored-by: Stephen Granade <stephen@granades.com>

--- server/src/common/parser.ts
+++ server/src/common/parser.ts
@@ -1,4 +1,4 @@
-import { Range, Location, Position, Diagnostic, DiagnosticSeverity } from 'vscode-languageserver/browser';
+import { Range, Location, Position, Diagnostic, DiagnosticSeverity } from 'vscode-languageserver';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
 import {
